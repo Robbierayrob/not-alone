7bec93265a4c0341c3e6443cee63acb167847b89
lisakhun58 <robert.bui404@gmail.com>
2024-11-11 23:34:35 +1100

    chore: Fix ESLint and TypeScript configuration issues

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,39 +5,6 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
-  rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
-  },
-};
-module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
     'eslint:recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
@@ -47,11 +14,13 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    project: ['tsconfig.json'],
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   ignorePatterns: [
     '/lib/**/*', // Ignore built files.
+    '*.js',
   ],
   plugins: [
     '@typescript-eslint',
